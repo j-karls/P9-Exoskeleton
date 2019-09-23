2c943f250dd75f72d24d9188d2ef30ca4a77131a
Unknown <Karlzone0@gmail.com>
2019-09-23 15:01:19 +0200

    Can compare svm regress vs linear regress

--- MIExperiments/Scikit-Learn/svmhhmTest.py
+++ MIExperiments/Scikit-Learn/svmhhmTest.py
@@ -3,9 +3,21 @@ import numpy as np
 from sklearn import datasets, linear_model, svm
 from sklearn.model_selection import train_test_split
 
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
+def sort_two_lists_as_one(main_list, other_list):
+    return (list(t) for t in zip(*sorted(zip(main_list, other_list))))
+    # Todo: Need unittests to ensure this works
+
+
 dia = datasets.load_diabetes()
 x, y = dia.data[:, np.newaxis, 2], dia.target
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+x_test, y_test = sort_two_lists_as_one(x_test, y_test)
 
 # Linear Regression
 lr_obj = linear_model.LinearRegression()
@@ -13,9 +25,13 @@ lr_obj.fit(x_train, y_train)
 lr_predict = lr_obj.predict(x_test)
 
 # Support-Vector-Regression
-svr_obj = svm.SVR(kernel="poly", degree=1, C=1000, gamma="auto", verbose=True)
+svr_obj = svm.SVR(kernel="poly", degree=2, C=10000, epsilon=0.01,
+                  gamma=0.9, cache_size=10, verbose=True)
 svr_obj.fit(x_train, y_train)
 svr_predict = svr_obj.predict(x_test)
+
+print("MAPE LR: " + str(mean_absolute_percentage_error(y_test, lr_predict)))
+print("MAPE SVR: " + str(mean_absolute_percentage_error(y_test, svr_predict)))
 
 plot.scatter(x_test, y_test, color="red", label="test set")
 plot.plot(x_test, lr_predict, color="blue", label="linear")
